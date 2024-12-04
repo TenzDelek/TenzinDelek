75378ad9708529730d9a624e1cc5fbde856c825d
TenzDelek <tibetdelek@gmail.com>
2024-12-04 18:52:05 +0530

    refactor: improve code formatting and readability in project page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,10 +1,10 @@
 import { ProjectDesign } from "@/components/Projects";
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 
 const Page = async (props: { params: Promise<{ id: string }> }) => {
   const params = await props.params;
-  const {id} = params;
+  const { id } = params;
   const cleanid = parseInt(decodeURIComponent(id), 10);
   const project = ProjectDesign.find((p) => p.id === cleanid);
 
@@ -22,10 +22,10 @@ const Page = async (props: { params: Promise<{ id: string }> }) => {
 
   return (
     <div className="max-w-4xl text-center mx-auto px-4">
-   <div className="fixed z-10">
-  <Link 
-    href="/" 
-    className="
+      <div className="fixed z-10">
+        <Link
+          href="/"
+          className="
       flex items-center
       px-4 py-2
       text-sm text-gray-700 
@@ -34,16 +34,14 @@ const Page = async (props: { params: Promise<{ id: string }> }) => {
       rounded-full
       transition-all duration-300
       hover:bg-opacity-30 "
-  >
-    <span className="mr-2">←</span>Home
-  </Link>
-</div>
+        >
+          <span className="mr-2">←</span>Home
+        </Link>
+      </div>
       <p className="text-lg">{project.name}</p>
-      <p className="font-applefontlight mb-4">
-        {project.description}
-      </p>
+      <p className="font-applefontlight mb-4">{project.description}</p>
       <Image
-        className="w-full"
+        className="w-full rounded-lg"
         src={project.imgss}
         placeholder="blur"
         width={800}
@@ -65,7 +63,9 @@ const Page = async (props: { params: Promise<{ id: string }> }) => {
         </div>
       </div>
       <p className="font-applefontlight">
-        Monlam Melong is one of the hero products for Monlam Manifest 2024. Designing it was a hurdle and a great learning experience. First comes the planning part.
+        Monlam Melong is one of the hero products for Monlam Manifest 2024.
+        Designing it was a hurdle and a great learning experience. First comes
+        the planning part.
       </p>
     </div>
   );
